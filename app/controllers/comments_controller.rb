fd0a76b5c70e3f203277991bc0dfc41bd78483e0
Rakesh Pankhania <rakesh@act.md>
2017-10-12 10:48:57 -0400

    Update: Render comments attributes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.all
+    @comments = Comment.all.order('created_at DESC')
   end
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+  def email_linkified_commenter_name(comment)
+    commenter_name = "#{comment.first_name} #{comment.last_name}"
+    return commenter_name if comment.email.blank?
+    link_to commenter_name, "mailto:#{comment.email}"
+  end
 end
