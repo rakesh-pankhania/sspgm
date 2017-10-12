6b1516419438f983310d1e9b996e16fd3eebfdf5
Rakesh Pankhania <rakesh@act.md>
2017-10-12 17:57:48 -0400

    Update: Render business attributes

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,7 +2,14 @@ class BusinessesController < ApplicationController
   before_action :set_business, only: [:show, :edit, :update, :destroy]
 
   def index
-    @businesses = Business.all
+    @businesses = Business.all.order(:name)
+
+    if params.key?(:category)
+      @filter_by = params[:category]
+      @businesses = @businesses.where(category: @filter_by)
+    else
+      @filter_categories = Business.all.order(:category).pluck('DISTINCT category')
+    end
   end
 
   def show
