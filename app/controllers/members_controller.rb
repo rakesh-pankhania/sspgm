1363fc8eb35449ddaa04111768c4c315f9e57b45
Rakesh Pankhania <rakesh@act.md>
2017-10-14 10:33:56 -0400

    Update: Render member attributes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,12 @@
 class MembersController < ApplicationController
   def index
-    @members = Member.all
+    @members = Member.all.order(:last_name, :first_name, :middle_name)
+
+    if params.key?(:country)
+      @filter_by = params[:country]
+      @members = @members.where(country: @filter_by)
+    else
+      @filter_categories = Member.all.order(:country).pluck('DISTINCT country')
+    end
   end
 end
--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,5 @@
 module MembersHelper
+  def member_name(member)
+    [member.first_name, member.middle_name, member.last_name].flatten.join(" ")
+  end
 end
