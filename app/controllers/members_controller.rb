dcc9d0ce88451d81aa8a14599227d68850e26f81
Rakesh Pankhania <rakesh@act.md>
2017-12-16 00:51:21 -0500

    Update: Clean up directory

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,12 +3,11 @@ class MembersController < ApplicationController
 
   def index
     @members = Member.all.order(:last_name, :first_name, :middle_name)
+    @filter_categories = Member.all.order(:country).pluck('DISTINCT country')
 
     if params.key?(:country)
       @filter_by = params[:country]
       @members = @members.where(country: @filter_by)
-    else
-      @filter_categories = Member.all.order(:country).pluck('DISTINCT country')
     end
   end
 end
