de03c6d424379077534f345155516bb86996114a
Rakesh Pankhania <rakesh.d.pankhania@gmail.com>
2020-06-28 17:28:57 -0400

    Unify top level paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     return url if url.starts_with? 'http://'
     "http://#{url}"
   end
+
+  def active_class_toggle_for(category)
+    active = case category
+             when :businesses then controller_path == "businesses"
+             when :professionals then controller_path == "professionals"
+             else false
+             end
+
+    active ? "active" : ""
+  end
 end
